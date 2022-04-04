13b75c6a446ec823ffec4ca233e297228714d8b6
jackieder <der.ja@husky.neu.edu>
2022-04-03 21:37:09 -0400

    add gitignore

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import helloController from "./controllers/hello-controller.js";
+import userController from "./controllers/users/user-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 const app = express();
+app.use(cors());
+app.use(express.json());
 helloController(app);
+userController(app);
+tuitsController(app);
 // app.get('/hello', (req, res) => {res.send('life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
